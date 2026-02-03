5184fb4173c6990a86a769d4b9d47d9825838b9e
Raksha D K <rakshadk9@gmail.com>
2026-02-03 13:50:43 +0530

    Add files via upload

---
+++ Taks2.py
@@ -0,0 +1,6 @@
+billamount=float(input("Total bill amount: "));
+totalpeople=int(input("Number of people: "));
+share=billamount/totalpeople;
+print(f"Total bill: {billamount}, Each person pays: {share}");
+print(type(billamount));
+print(type(share));
