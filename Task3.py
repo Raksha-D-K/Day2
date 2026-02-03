0884c58413bec8b25fc6d9f5d38513d05e5f46fe
Raksha D K <rakshadk9@gmail.com>
2026-02-03 13:51:14 +0530

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,7 @@
+item_name= "keyboard";
+quantity= 14;
+price= 50000;
+in_stock= True;
+print(f"Item: {item_name}, Quantity: {quantity}, Price: {price}, Available: {in_stock}");
+totalcost= quantity * price;
+print(totalcost);
